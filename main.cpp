1fc83578bbc3ba2409ecce6847a80c265cfc6aee
marekcieplik <marek.cieplik@gmail.com>
2014-05-19 12:05:58 +0200

    nie dziala

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-#include "FizzBuzzer.h"
+//#include "FizzBuzzer.h"
+#include "FizzBuzzerIs5Or3.h"
 
 using namespace std;
 
 int main()
 {
-	FizzBuzzer f(1,15);
-
+	//FizzBuzzerIs5Or3 f(1,15);
+    FizzBuzzerIs5Or3 f(1,15);
 	while (f.hasNextString()){
 		cout << f.getNextString() << endl;
 	}
-	
+
     return 0;
 }
 
